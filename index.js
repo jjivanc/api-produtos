52211bac766466e87582b7478e1337333d02dcd3
jjivanc <62190299+jjivanc@users.noreply.github.com>
2021-09-25 22:49:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const { v4: uuid } = require('uuid')
+
+const server = express()
+
+server.use(express.json())
+
+/* Informações para o Produto
+    "titulo": "Notebook Lenovo Ideapad ",
+	"descrição": "Notebook Lenovo Ideapad S145 81V70008BR - AMD Ryzen 5-3500U 8GB 256GB SSD 15,6” Windows 10",
+	"valor": 3419,
+	"marca": "Lenovo",
+	"modelo": "Ideapad",
+	"promocao": true,
+	"estoque": 30
+*/
+
+const produtos = [{
+    "titulo": "Notebook Lenovo Ideapad ",
+	"descrição": "Notebook Lenovo Ideapad S145 81V70008BR - AMD Ryzen 5-3500U 8GB 256GB SSD 15,6” Windows 10",
+	"valor": 3419,
+	"marca": "Lenovo",
+	"modelo": "Ideapad",
+	"promocao": true,
+	"estoque": 30
+}]
+
+//GET => http://localhost:3000/produtos
+server.get('/', (req,res) =>{
+    return res.sendFile(__dirname+"/html/index.html");
+})
+// GET / => Retorna todos os protudos
+server.get('/produtos', (req, res) => {
+  return res.send(produtos)
+})
+
+server.post('/produtos', (req, res) => {
+  const dados = req.body
+
+  produtos.push({
+    id: uuid(),
+    ...dados,
+  })
+
+  return response.json({ mensagem: 'Produto cadastrado com sucesso!' })
+})
+
+server.put('/produto/:id', (req, res) => {
+  const id_produto = req.params.id
+  const dados = req.body
+
+  const produtoIndex = produtos.findIndex((produto) => {
+    return produto.id === id_produto
+  })
+
+  if (produtoIndex === -1) {
+    return res.send({ mensagem: 'Professor não encontrado' })
+  }
+
+  produtos[produtoIndex] = {
+    id: id_produto,
+    ...dados,
+  }
+
+  return response
+    .status(200)
+    .send({ mensagem: 'Produto atualizado com sucesso' })
+})
+
+server.delete('/produtos/:id', (req, res) => {
+  const id_produto = req.params.id
+
+  const produtoIndex = produtos.findIndex((produto) => {
+    return produto.id === id_produto
+  })
+
+  if (produtoIndex === -1) {
+    return res.send({ mensagem: 'Produto não encontrado' })
+  }
+  produtos.splice(produtoIndex, 1)
+
+  return res.json({ mensagem: 'Produto apagado com sucesso!' })
+})
+
+
+server.listen(3000, () => {
+  console.log('Server rodando na porta 3000')
+})
